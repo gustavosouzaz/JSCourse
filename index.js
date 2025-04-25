78311ae0e74750308cb847ae422a28b742127736
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-04-25 14:22:36 -0300

    tipos primitivos

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ console.log(nome);
 const interestRate = 0.3;
 interestRate = 1;
 console.log(interestRate);
+
+
+let nomee = 'Mosh' // String
+let age = 30; // Int ou Double
+let isApproved = false; // Boolean
+let firstName = undefined; 
+let selectColor = null;
