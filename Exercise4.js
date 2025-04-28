4003a5de30287660d35cf4081f500cba8c79369d
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-04-28 15:12:42 -0300

    Fourth Exercise

---
+++ Exercise4.js
@@ -0,0 +1,12 @@
+checkSpeed(130);
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+
+    if(speed < speedLimit + kmPerPoint) {
+        console,log('OK');
+        return;
+    }
+    //
+}
