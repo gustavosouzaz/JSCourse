8db924a8b27d21d197d5c351b04af6b7badcc4f6
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-04-28 13:22:07 -0300

    Secçao de funções,arrays e ect

--- index.js
+++ index.js
@@ -14,3 +14,37 @@ let age = 30; // Int ou Double
 let isApproved = false; // Boolean
 let firstName = undefined; 
 let selectColor = null;
+
+//Object
+
+let person = {
+    name: 'Mosh',
+    age: 30
+};
+
+person.name = 'Gustavo';
+
+
+//Arrays
+
+let selectdColor = ['red','blue'];;
+selectdColor[2] = 1;
+console.log(selectedColors[0]);
+
+
+//Function
+
+
+function greet(nameee) {
+    console.log('Hello' + nameee);
+};
+
+greet('Gustavo');
+greet('Gu');
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
