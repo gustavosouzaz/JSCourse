898eb07372f58098b6b075fbc4731861d83ae6e1
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-04-28 14:41:38 -0300

    Loops

--- index2.js
+++ index2.js
@@ -4,5 +4,30 @@ if (hour >= 6 && hour < 12) {
     console.log('Good morning')
 }else if (hour >= 12 && hour < 12) {
     console.log('Good Afternoon')
+} else onsole.log('Good evening')
 
+
+
+
+// FOR
+
+for (let i = 0; i < 5; i++) {
+console.log('Hello world')
 }
+
+
+//while
+
+//let i = 9;
+while(i <= 5) {
+    if (i % 2 !== 0)
+        i++
+}
+
+//do-while
+
+let i = 9;
+do {
+    if (i % 2 !== 0) console.log(i);;
+    i++
+}while (i <= 5);
