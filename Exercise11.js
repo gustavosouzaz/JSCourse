22f15a2895cf7ae21cf3e58dee812dcdf839d81e
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-04-29 14:28:31 -0300

    Exercise Eleven

---
+++ Exercise11.js
@@ -0,0 +1,12 @@
+let post = {
+    title: 'a',
+    body: 'b',
+    author: 'c',
+    comments: [
+        { author: 'a', body: 'b'},
+        { author: 'c', body: 'd'},
+    ],
+    isLive: true
+};
+
+console.log(post);
