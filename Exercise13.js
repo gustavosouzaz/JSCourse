10ddb361b56626a72c3178338fc446a1d0b5d6f3
gustavosouzaz <gustavodsouzaaa@gmail.com>
2025-04-29 14:35:37 -0300

    Thirteen

---
+++ Exercise13.js
@@ -0,0 +1,9 @@
+let priceRanges = [
+    { label: '$', tooltip: 'Inexpensive', minPerPerson: 0, maxPerson: 10},
+    { label: '$$', tooltip: 'Moderate', minPerPerson: 11, maxPerson: 20},
+    { label: '$$$', tooltip: 'Expensive', minPerPerson: 21, maxPerson: 50},
+];
+
+let restaurants = [
+    {averagePerPerson: 5}
+]
